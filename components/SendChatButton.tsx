8dabcd296f6c9e95c74537d085458631aa0e632d
Hunter Kalinoski <hunterkalinoski@gmail.com>
2023-01-08 16:28:07 -0500

    Button to send message disappears if not signed in

--- components/SendChatButton.tsx
+++ components/SendChatButton.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { auth } from "@lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 export default function Page({}) {
-  return (
+  const [authState] = useAuthState(auth);
+
+  return authState ? (
     <button>
       <p>Send</p>
     </button>
+  ) : (
+    <div className="p-0 text-center text-gray-200">Sign In to send messages!</div>
   );
 }
