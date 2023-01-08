d0a582bfafa518caa34073817ecc3a9b6d706194
Hunter Kalinoski <hunterkalinoski@gmail.com>
2023-01-08 16:05:06 -0500

    message feed displays messages from firestore

--- components/MessageFeed.tsx
+++ components/MessageFeed.tsx
@@ -1,25 +1,34 @@
-export default function Page({}) {
-  // fetch 10 most recent posts
-  // map posts to display them
+"use client";
 
-  return (
-    <div className="max-h-[36rem] w-5/6 overflow-y-scroll rounded-lg bg-slate-600 p-5">
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-      <p className="m-10 bg-slate-500">Message feed</p>
-    </div>
+import { firestore } from "@lib/firebase";
+import { collection, limit, orderBy, query } from "firebase/firestore";
+import { useEffect } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+export default function MessageFeed({}) {
+  const messagesQuery = query(
+    collection(firestore, "messages"),
+    orderBy("createdAt", "desc"),
+    limit(10)
   );
+  const [collectionSnapshot, loading, error] = useCollection(messagesQuery);
+
+  const MessagesComponent = () => {
+    if (collectionSnapshot) {
+      return (
+        <div className="flex max-h-[36rem] w-5/6 flex-col gap-4 overflow-y-scroll rounded-lg bg-slate-800 p-10">
+          {collectionSnapshot?.docs.map((doc) => (
+            <div className="w-4/5 rounded-lg bg-slate-700 p-4 text-gray-200" key={doc.id}>
+              <h3 className=" place-items-start justify-self-end text-2xl">{doc.data().author}</h3>
+              <p>{doc.data().content}</p>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return <div>No messages!</div>;
+    }
+  };
+
+  return <MessagesComponent />;
 }
