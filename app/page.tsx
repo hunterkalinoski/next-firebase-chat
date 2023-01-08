51213e7352754c118ee5781bdc2010e42a9b4529
Hunter Kalinoski <hunterkalinoski@gmail.com>
2023-01-08 15:32:50 -0500

    Layout for main page (chat stuff)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import Counter from "@components/Counter";
+import MessageFeed from "@components/MessageFeed";
+import SendChatButton from "@components/SendChatButton";
 
 export default function Home() {
   return (
-    <main className="bg-slate-700">
-      <div>Hello new next project with firebase + tailwind + typescript</div>
-      <Counter />
+    <main className="flex flex-col items-center gap-8 bg-slate-700 pt-8">
+      <MessageFeed />
+      <section className="flex w-5/6 items-center justify-center gap-8">
+        <textarea className="h-32 w-full" />
+        <SendChatButton />
+      </section>
     </main>
   );
 }
---
+++ components/MessageFeed.tsx
@@ -0,0 +1,25 @@
+export default function Page({}) {
+  // fetch 10 most recent posts
+  // map posts to display them
+
+  return (
+    <div className="max-h-[36rem] w-5/6 overflow-y-scroll rounded-lg bg-slate-600 p-5">
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+      <p className="m-10 bg-slate-500">Message feed</p>
+    </div>
+  );
+}
